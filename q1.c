8c2bd15bfeef5fd5c7e3104d2084be52152ec6aa
Allen <c130914@gmail.com>
2023-04-29 16:23:33 +0800

    測試main

--- q1.c
+++ q1.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
-#include <math.h>
+#include "lib/route.h"
+#include "lib/board.h"
 
-void main()
+int main()
 {
-    int a=2, b =3, c;
+    BOARD *board;
+    ROUTE *route, *next;
 
-    c = pow(2, 3);
-    printf("%d", c);
+    board = board_create(10, 10);
+    // board_destory(board);
+    // 新路線從 (0,0) 開始
+    route = route_create(0, 0, 1);
+    // 加一個下一步 (1,1)
+    next = route_next(route, 1, 1);
+    // 加一個下一步 (2,2)
+    next = route_next(next, 2, 2);
+    // 加一個下一步 (3,3)
+    next = route_next(next, 3, 3);
+
+    // 印出所有步驟
+    route_print(route);
+
+    // clean up memory
+    route_destory(route);
+
+    return 0;
 }

206e6d793e8cfe5b926d8696c7eefdecddbb2163
adamcfd <91072420+adamcfd@users.noreply.github.com>
2023-04-30 02:45:03 +0000

    fix comments

--- lib/route.c
+++ lib/route.c
@@ -2,30 +2,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// destroy the route
 void route_destory(ROUTE *route)
 {
+    if(route->prev)
+    {
+        route->prev->next = NULL;
+    }
+
     if (route->next)
     {
         route_destory(route->next);
         free(route->next);
         route->next = NULL;
     }
+
+    free(route);
 }
 
-// 建立新的路線於 (x, y)
+// create route start with (x, y) and a stepNo
 ROUTE *route_create(unsigned x, unsigned y, unsigned stepNo)
 {
     ROUTE *route;
 
     route = malloc(sizeof(ROUTE));
-    route->stepNo = stepNo;  // 第 1 步
+    route->stepNo = stepNo;
     route->x = x;
     route->y = y;
+    route->next = NULL;    
 
     return route;
 }
 
-// 印出路線的座標
+// print the route
 void route_print(ROUTE *route)
 {
     printf("#%d (%d, %d)\n", route->stepNo, route->x, route->y);
@@ -35,7 +44,7 @@ void route_print(ROUTE *route)
     }
 }
 
-// 路線加入下一步
+// add next one to the route
 ROUTE *route_next(ROUTE *route, unsigned x, unsigned y)
 {
     ROUTE *next = route_create(x, y, route->stepNo + 1);
--- lib/route.h
+++ lib/route.h
@@ -1,34 +1,34 @@
 #ifndef __ROUTE_H__
 #define __ROUTE_H__
 
-// 一個路線結構
+// ROUTE
 typedef struct ROUTE ROUTE;
 
 struct ROUTE
 {
-    // 此路線的 next 指向下一步的路線, 簡單說就是個 LinkedList 資料結構
+    // pointer to next ROUTE
     ROUTE *next;
-    // 此路線的 prev 指向前一步的路線, 用於無路可走的時候往前搜尋
+    // pointer to previous ROUTE
     ROUTE *prev;
 
-    // x, y 代表目前路線使用到的座標, 要與棋盤座標系配合才能得知他再棋盤上的一維地址
+    // (x, y)
     int x;
     int y;
 
-    // 第幾步
+    // No. of this step
     unsigned stepNo;
 };
 
-// 清除路線
+// destroy the route
 void route_destory(ROUTE *route);
 
-// 建立新的路線於 (x, y)
+// create route start with (x, y) and a stepNo
 ROUTE *route_create(unsigned x, unsigned y, unsigned stepNo);
 
-// 印出路線的座標
+// print the route
 void route_print(ROUTE *route);
 
-// 路線加入下一步
+// add next one to the route
 ROUTE *route_next(ROUTE *route, unsigned x, unsigned y);
 
 #endif

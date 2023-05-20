51a1d71162f8b1c1bdccfbe2c45b72a362fca240
Adam Chen <adamchen.cfd@gmail.com>
2023-05-20 13:57:05 +0800

    偶x偶建檔

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include "lib/route.h"
 #include "lib/board.h"
 #include "lib/board_4kx4k.h"
+#include "lib/board_4k2x4k.h"
+#include "lib/board_4kx4k2.h"
+#include "lib/board_4k2x4k2.h"
 
-int main_2()
+int main()
 {
     int m = 0, n = 0;
     while(m * n == 0)
@@ -21,14 +24,60 @@ int main_2()
         }
     }
 
-    if(m % 4 == 0 && n % 4 == 0)
+    if(m % 4 == 0 && n % 4 == 0)// 4k x 4k
     {
         BOARD *totalBoard;
 
-        totalBoard = board_4kx4k(n, m);
+        totalBoard = board_4kx4k(m, n);
+
+        board_print(totalBoard);
 
         board_svg(totalBoard);
     }
+
+    if(m % 4 == 0 && n % 4 == 2)// 4k x 4k2
+    {
+        BOARD *totalBoard;
+
+        //totalBoard = board_4kx4k2(m, n);
+
+        board_print(totalBoard);
+
+        board_svg(totalBoard);
+    }
+
+    if(m % 4 == 2 && n % 4 == 0)// 4k2 x 4k
+    {
+        BOARD *totalBoard;
+
+        //totalBoard = board_4k2x4k(m, n);
+
+        board_print(totalBoard);
+
+        board_svg(totalBoard);
+    }
+
+    if(m % 4 == 2 && n % 4 == 2)// 4k2 x 4k2
+    {
+        BOARD *totalBoard;
+
+        //totalBoard = board_4k2x4k2(m, n);
+
+        board_print(totalBoard);
+
+        board_svg(totalBoard);
+    }
+
+    /*
+    if(m % 4 == 2 && n % 4 == 1)// (4k+2) x (4k+1)
+    {
+        BOARD *totalboard;
+
+        totalboard = board_4k2x4k1(m, n);
+
+        board_print(totalboard);
+    }
+    */
 
     return 0;
 }

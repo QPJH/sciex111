399aa316d50b8c55a151a2d11dd593a4af7758d0
adamcfd <91072420+adamcfd@users.noreply.github.com>
2023-05-30 09:15:22 +0000

    adam_dfs

--- lib/adam.c
+++ lib/adam.c
@@ -3,76 +3,6 @@
 #include "board.h"
 #include "adam.h"
 
-int completedCount = 0;
-int completed[1000000][128];
-
-int *board_getCompleted(BOARD *board)
-{
-    int i, j, length, used;
-    ROUTE *r;
-
-    length = board_length(board);
-    r = route_last(board->route);
-
-    if (r->stepNo < 16)
-    {
-        return NULL;
-    }
-
-    for (i = 0; i < completedCount; i++)
-    {
-        if (r->stepNo == completed[i][0])
-        {
-            used = 0;
-            for (j = 0; j < length; j++)
-            {
-                if (board->grids[j])
-                {
-                    if (completed[i][used + 1] != j)
-                    {
-                        break;
-                    }
-                    used ++;
-                }
-            }
-
-            if (j == length)
-            {
-                // every grids all are same
-                return completed[i];
-            }
-        }
-    }
-
-    return NULL;
-}
-
-void board_markCompeleted(BOARD *board)
-{
-    ROUTE *r;
-    int i, used, length;
-
-    length = board_length(board);
-    r = route_last(board->route);
-
-    if( r->stepNo < 16)
-    {
-        return;
-    }
-    completed[completedCount][0] = r->stepNo;
-
-    used = 0;
-    for (i = 0; i < length; i++)
-    {
-        if (board->grids[i])
-        {
-            completed[completedCount][used + 1] = i;
-            used ++;
-        }
-    }
-    completedCount++;
-}
-
 int maxLength = 0;
 ROUTE *maxRoute = NULL;
 ROUTE *last = NULL;
@@ -91,7 +21,7 @@ adam_dfs(BOARD *board)
     unsigned length, i, used = 0;
     int x, y;
     BOARD *b;
-    ROUTE *r;
+    ROUTE *r, *rLast;
 
     if(!board->route)
     {
@@ -102,9 +32,10 @@ adam_dfs(BOARD *board)
 
     b = board_copy(board);
 
+    rLast = route_last(b->route);
     for(i = 0; i < length; i++)
     {
-        r = route_last(b->route);
+        r = rLast;
 
         if( b->grids[i] )
         {
@@ -153,15 +84,25 @@ adam_dfs(BOARD *board)
         }
 
     }
-    route_destory(b->route);
-    board_destory(b);
 
     // no next move;
+    if ( rLast->stepNo > maxLength )
+    {
+        if( (maxRoute)) {
+            route_destory(maxRoute);
+        }
+        maxRoute = route_copy(b->route);
+        maxLength = rLast->stepNo;
+    }
+
     if(length == used)
     {
         board_print(board);
     }
 
+    route_destory(b->route);
+    board_destory(b);
+    
     return board;
 }
 
